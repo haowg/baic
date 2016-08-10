b51e0af4525405cadd315dd3dc381ba84e5c1e1b
haowg <haoweiguoyouxiang@163.com>
2016-08-10 21:32:16 +0800

    添加useragentmiddleware

--- spider/baic/settings.py
+++ spider/baic/settings.py
@@ -52,9 +52,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'baic.middlewares.MyCustomSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'baic.middlewares.UseragentMiddleware.RandomUserAgentMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
